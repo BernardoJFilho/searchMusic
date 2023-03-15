bb4598dc6f4c1c3a814ab5e021e8f0b4a43b7c60
BernardoJFilho <bernardojcm.filho@gmail.com>
2023-03-15 18:54:13 -0300

    termina questao5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,48 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    pesquisar: '',
+    buttonSearch: true,
+  };
+
+  buttonEnable = ({ target }) => {
+    if (target.value.length >= 2) {
+      this.setState({
+        [target.name]: target.value,
+        buttonSearch: false,
+      });
+    } else {
+      this.setState({
+        [target.name]: target.value,
+        buttonSearch: true,
+      });
+    }
+  };
+
   render() {
+    const { buttonSearch, pesquisar } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <div>Search</div>
+        <div>
+          <label>
+            <input
+              name="pesquisar"
+              type="text"
+              placeholder="Buscar"
+              value={ pesquisar }
+              onChange={ this.buttonEnable }
+              data-testid="search-artist-input"
+            />
+          </label>
+          <button
+            disabled={ buttonSearch }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
