afd8bf4720e0e8cbd1fd0e1d72b74285e02a9a3d
BernardoJFilho <bernardojcm.filho@gmail.com>
2023-03-15 18:42:53 -0300

    termina questao4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -23,6 +24,17 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <div data-testid="header-user-name">{ nameId }</div>
+        <div>
+          <Link to="/search" data-testid="link-to-search">
+            <button>Search</button>
+          </Link>
+          <Link to="/favorites" data-testid="link-to-favorites">
+            <button>Favorites</button>
+          </Link>
+          <Link to="/profile" data-testid="link-to-profile">
+            <button>Profile</button>
+          </Link>
+        </div>
       </header>
     );
   }
